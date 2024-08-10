7f5c7feb4bc46b76c2a3827a8c408b24dd5b67df
Nikita Yakovlev <nikita.yakovlev@spl.games>
2024-08-10 23:17:33 +0300

    poetry init

---
+++ scripts.py
@@ -0,0 +1,25 @@
+import logging
+import pathlib
+import sys
+import black
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+top_level_dir = pathlib.Path(__file__).parent
+
+
+def lint():
+    if len(sys.argv) > 1:
+        logger.warning("lint not support arguments")
+        logger.warning("Ignoring arguments: %s", sys.argv[1:])
+
+    sys.argv = [
+        "black",
+        str(top_level_dir / "weathebot"),
+        "--config",
+        "./pyproject.toml",
+        "--diff",
+        "--color",
+    ]
+    black.patched_main()
